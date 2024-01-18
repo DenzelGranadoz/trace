896b79660c28fe3d28e1c40418720dc9f03a5f27
DenzelGranadoz <zelgranadoz@gmail.com>
2024-01-18 15:14:23 +0800

    Removed progress display in ticket cards

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,50 +1,48 @@
-import DeleteBlock from "./DeleteBlock";
-import PriorityDisplay from "./PriorityDisplay";
-import ProgressDisplay from "./ProgressDisplay";
-import StatusDisplay from "./StatusDisplay";
+import DeleteBlock from './DeleteBlock';
+import PriorityDisplay from './PriorityDisplay';
+import ProgressDisplay from './ProgressDisplay';
+import StatusDisplay from './StatusDisplay';
 
-import Link from "next/link";
+import Link from 'next/link';
 
 const TicketCard = ({ ticket }) => {
-  const { _id, title, description, priority, progress, status, createdAt } =
-    ticket;
+  const { _id, title, description, priority, status, createdAt } = ticket;
 
   const formatTimestamp = (timestamp) => {
     const options = {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
       hour12: true,
     };
 
     const date = new Date(timestamp);
-    const formattedDate = date.toLocaleString("en-US", options);
+    const formattedDate = date.toLocaleString('en-US', options);
 
     return formattedDate;
   };
 
   return (
-    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
-      <div className="flex mb-3">
+    <div className='flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2'>
+      <div className='flex mb-3'>
         <PriorityDisplay priority={priority} />
-        <div className="ml-auto">
+        <div className='ml-auto'>
           <DeleteBlock id={_id} />
         </div>
       </div>
 
-      <Link href={`/TicketPage/${_id}`} style={{ display: "contents" }}>
+      <Link href={`/TicketPage/${_id}`} style={{ display: 'contents' }}>
         <h4>{title}</h4>
-        <hr className="h-px border-0 bg-page mb-2" />
-        <p className="whitespace-pre-wrap">{description}</p>
-        <div className="flex-grow"></div>
-        <div className="flex mt-2">
-          <div className="flex flex-col">
-            <p className="text-xs my-1">{formatTimestamp(createdAt)}</p>
-            <ProgressDisplay progress={progress} />
+        <hr className='h-px border-0 bg-page mb-2' />
+        <p className='whitespace-pre-wrap'>{description}</p>
+        <div className='flex-grow'></div>
+        <div className='flex mt-2'>
+          <div className='flex flex-col'>
+            <p className='text-xs my-1'>{formatTimestamp(createdAt)}</p>
           </div>
-          <div className="ml-auto flex items-end">
+          <div className='ml-auto flex items-end'>
             <StatusDisplay status={status} />
           </div>
         </div>
