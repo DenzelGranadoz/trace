9ed711f127c87af070994d85e2b03176c8ba330a
DenzelGranadoz <zelgranadoz@gmail.com>
2024-01-19 16:35:01 +0800

    custom login page

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -50,23 +50,25 @@ const Login = () => {
     if (!res.ok) {
       setErrorMessage(res.error);
     } else {
-      router.refresh();
       router.push('/');
+      router.refresh();
     }
   };
 
   return (
     <div className='w-full h-full flex justify-center items-center bg-gray-200'>
-      <div className='lg:w-1/2 sm:w-3/4 xl:w-1/3  h-3/5 border p-20 flex flex-col justify-between bg-gray-500'>
+      <div className='lg:w-1/2 sm:w-3/4 xl:w-1/3  h-3/5 border p-20 flex flex-col justify-between bg-gray-100'>
         <div>
-          <h1 className='text-center text-5xl '>Welcome Back</h1>
-          <h4 className='text-center '>Please log in to continue</h4>
+          <h1 className='text-center text-5xl text-slate-600'>Welcome Back</h1>
+          <h4 className='text-center text-slate-600'>
+            Please log in to continue
+          </h4>
         </div>
         <form
           className='flex flex-col align-center justify-center p-0'
           onSubmit={handleSubmit}
         >
-          <label className='mb-1.5 text-black'>Email Address</label>
+          <label className='mb-1.5 text-slate-600'>Email Address</label>
           <input
             id='email'
             name='email'
@@ -75,9 +77,9 @@ const Login = () => {
             onChange={handleChange}
             value={formData.email}
             required
-            className='block bg-gray-300 m-0 px-4 py-3 w-full rounded-none text-xl text-black focus:ring-green-500 focus:rounded-none'
+            className='block bg-gray-200 m-0 px-4 py-3 w-full rounded-none text-xl text-black focus:ring-green-500 focus:rounded-none'
           />
-          <label className='mb-1.5 text-black'>Password</label>
+          <label className='mb-1.5 text-slate-600'>Password</label>
           <input
             id='password'
             name='password'
@@ -86,29 +88,43 @@ const Login = () => {
             onChange={handleChange}
             value={formData.password}
             required
-            className='block bg-gray-300 m-0 px-4 py-3 w-full rounded-none text-xl text-black focus:ring-green-500 focus:rounded-none'
+            className='block bg-gray-200 m-0 px-4 py-3 w-full rounded-none text-xl text-black focus:ring-green-500 focus:rounded-none'
           />
-          <Link href='/Register' className='w-full text-right mt-6 mb-3.5'>
-            <span>Forget Password?</span>
-          </Link>
+
+          <div className='w-full text-right mt-6 mb-3.5  flex justify-end '>
+            <Link
+              href='/Register'
+              className='text-blue-400 hover:text-slate-400'
+            >
+              Forget password?
+            </Link>
+          </div>
           <input
             type='submit'
             value='Login'
-            className='m-0 p-3 w-full bg-blue-400 hover:bg-blue-100'
+            className='m-0 p-3 w-full bg-blue-400 hover:bg-blue-100 hover:cursor-pointer'
           />
-          {errorMessage && <p>{errorMessage}</p>}
+          {errorMessage && <p className='text-red-400 my-2'>{errorMessage}</p>}
         </form>
         <div className='h-20 flex flex-col text-center'>
-          <p>Or log in with:</p>
+          <p className='text-slate-600'>Or log in with:</p>
           <div className='flex-1 flex pt-2'>
-            <button className='flex-1 border-white border'>Github</button>
-            <button className='flex-1 border-white border'>Google</button>
-            <button className='flex-1 border-white border'>Discord</button>
+            <button className='flex-1 border-white border bg-gray-400 hover:bg-gray-500'>
+              Github
+            </button>
+            <button className='flex-1 border-white border bg-gray-400 hover:bg-gray-500'>
+              Google
+            </button>
+            <button className='flex-1 border-white border bg-gray-400 hover:bg-gray-500'>
+              Discord
+            </button>
           </div>
         </div>
-        <div className='h-5 w-full flex justify-center'>
-          <span>No account yet?</span>
-          <Link href='/Register'> Register Here</Link>
+        <div className='h-5 w-full flex justify-center '>
+          <span className='text-slate-600 mr-1'>No account yet?</span>
+          <Link href='/Register' className='text-blue-400 hover:text-slate-400'>
+            Register Here
+          </Link>
         </div>
       </div>
     </div>
