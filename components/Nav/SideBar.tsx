86fe4d6a7c7ecf0ea46a9f3052b348d39ccd6381
Denzel Granadoz <zelgranadoz@gmail.com>
2024-02-29 21:19:04 +0800

    fix: display login/logout btn if session exist or not

--- components/Nav/SideBar.tsx
+++ components/Nav/SideBar.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 import NavItem from './NavItem';
 import { sideBarDetails } from './constants';
 
+import { getServerSession } from 'next-auth';
+import { options } from '@/app/api/auth/[...nextauth]/options';
+
 interface SideBarProps {}
 
-const SideBar: React.FC<SideBarProps> = ({}) => {
+const SideBar: React.FC<SideBarProps> = async ({}) => {
   const { heading, navs } = sideBarDetails;
+  const session = await getServerSession(options);
   return (
     <section className="h-full w-1/4 flex flex-col justify-between pr-8">
       <div className="flex flex-col gap-16">
@@ -25,12 +29,21 @@ const SideBar: React.FC<SideBarProps> = ({}) => {
       </div>
 
       <div>
-        <NavItem
-          href="/Login"
-          // icon={navItem.icon}
-          index={4}
-          label="Log in"
-        />
+        {session ? (
+          <NavItem
+            href="/Logout"
+            // icon={navItem.icon}
+            index={5}
+            label="Log out"
+          />
+        ) : (
+          <NavItem
+            href="/Login"
+            // icon={navItem.icon}
+            index={4}
+            label="Log in"
+          />
+        )}
       </div>
     </section>
   );
