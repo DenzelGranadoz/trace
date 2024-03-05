ee745083497e2074bcfcd151e9625fda908dc2b1
Denzel Granadoz <zelgranadoz@gmail.com>
2024-03-05 22:20:48 +0800

    fix: added validation to redirect if no session exist

--- utils/session.ts
+++ utils/session.ts
@@ -1,6 +1,13 @@
 import { getServerSession } from 'next-auth';
 import { options } from '@/app/api/auth/[...nextauth]/options';
+import { redirect } from 'next/navigation';
 
 export async function getSession() {
-  return await getServerSession(options);
+  const session = await getServerSession(options);
+
+  if (!session) {
+    redirect('/About');
+  }
+
+  return session;
 }
