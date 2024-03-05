70421b3c0c18b6a0694fabf1f72bfc154e7d06f9
Denzel Granadoz <zelgranadoz@gmail.com>
2024-03-05 22:21:09 +0800

    fix: add another request

--- utils/requests.ts
+++ utils/requests.ts
@@ -1,10 +1,9 @@
-import { getServerSession } from 'next-auth';
-import { options } from '@/app/api/auth/[...nextauth]/options';
 import { NextResponse } from 'next/server';
+import { getSession } from './session';
 
-export const getTickets = async () => {
-  const session = await getServerSession(options);
+export const getTickets = async (): Promise<any> => {
   try {
+    const session = await getSession();
     const { email } = session.user;
 
     const res = await fetch(
@@ -14,8 +13,35 @@ export const getTickets = async () => {
       }
     );
 
+    if (!res.ok) {
+      throw new Error('Failed to fetch tickets');
+    }
+
     return res.json();
   } catch (error) {
-    return NextResponse.json({ message: 'Error', error }, { status: 500 });
+    return NextResponse.json(
+      { message: 'Error fetching tickets' },
+      { status: 500 }
+    );
+  }
+};
+
+export const getTicketById = async (id: string): Promise<any> => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      cache: 'no-store',
+    });
+
+    if (!res.ok) {
+      throw new Error('Ticket not found');
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error('Error fetching ticket by ID:', error);
+    return NextResponse.json(
+      { message: 'Error fetching ticket by ID' },
+      { status: 500 }
+    );
   }
 };
